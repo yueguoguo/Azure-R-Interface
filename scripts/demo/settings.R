3db9c25b1b4370810969b71488b440348757106d
Graham Williams <graham.williams@togaware.com>
2016-12-22 11:14:16 +0800

    Cleanup.

--- scripts/demo/settings.R
+++ scripts/demo/settings.R
@@ -9,7 +9,8 @@
 ########################################################################
 
 VM_PUBKEY <- "ssh-rsa AAAAB3NzaC1yc2E...1FD" # OpenSSH compatible public key.
-RG        <- "my_dsvm_rg_sea" # Resource group as  manually created in Azure.
-TID       <- "88bf...011d" # Tenant ID obtained during app creation in Active Directory.
-CID       <- "10e3....d3d1" # Client ID obtained during app creation in Active Directory.
-KEY       <- "u/cc....53hg" # User key obtained during app creation in Active Directory.
+RG        <- "my_dsvm_rg_sea" # Resource group as manually created in Azure.
+LOC       <- "southeastasia"  # Data centre location for new resource group.
+TID       <- "88bf...011d"    # Tenant ID from app creation in Active Directory.
+CID       <- "10e3....d3d1"   # Client ID from app creation in Active Directory.
+KEY       <- "u/cc....53hg"   # User key from app creation in Active Directory.
