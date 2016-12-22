e6c775b7772bcd71203384ece3df487c9f47256e
Graham Williams <graham.williams@togaware.com>
2016-12-22 08:37:45 +0800

    Initial import

---
+++ scripts/demo/settings.R
@@ -0,0 +1,15 @@
+########################################################################
+# Seetings for Managing Azure Resorces
+# ----------------------------------------------------------------------
+# AUTHORS:            Le Zhang, Graham Williams.
+# CONTRIBUTORS:       Le Zhang, Graham Williams.
+# DATE OF CREATION:   20161222
+# DEPARTMENT:         ADS Asia
+# COMPANY:            Microsoft
+########################################################################
+
+VM_PUBKEY     <- # OpenSSH compatible public key.
+RG            <- # Resource group. NOTE: should be manually created.
+TID           <- # Tenant ID. NOTE: obtained in creating app in Active Directory.
+CID           <- # Client ID. NOTE: obtained in creating app in Active Directory.
+KEY           <- # User key. NOTE: obtained in creating app in Active Directory.
