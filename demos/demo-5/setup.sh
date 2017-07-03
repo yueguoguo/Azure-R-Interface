5017b55c8fd968a3041414105fb13b2ce98bdc49
Zhang Le <yueguoguo@users.noreply.github.com>
2017-07-03 11:28:32 +0800

    Update setup.sh

--- demos/demo-5/setup.sh
+++ demos/demo-5/setup.sh
@@ -7,20 +7,20 @@ sudo Rscript -e 'library(devtools);devtools::install_github("rstudio/reticulate"
 # downgrade CNTK. CNTK2.0RC1 is pre-installed but it does not have
 # attribute set_global_option for initializing properly.
 
-sudo /anaconda/envs/py35/bin/pip install https://cntk.ai/PythonWheel/GPU/cntk-2.0-cp35-cp35m-linux_x86_64.whl
+# sudo /anaconda/envs/py35/bin/pip install https://cntk.ai/PythonWheel/GPU/cntk-2.0-cp35-cp35m-linux_x86_64.whl
 
 # upgrade keras. By default keras R package does not install the
 # latest version so there is no CNTK backend supported.
 
-sudo /anaconda/envs/py35/bin/pip install --upgrade keras 
+# sudo /anaconda/envs/py35/bin/pip install --upgrade keras 
 
 # make a keras config file
 
 echo '{"floatx":"float32","image_data_format":"channels_last","epsilon":1e-07,"backend":"cntk"}' > ~/.keras/keras.json
 
 # add environment variables.
-export KERAS_BACKEND="cntk"
-export KERAS_PYTHON="/anaconda/envs/py35/bin/python3.5"
+# export KERAS_BACKEND="cntk"
+# export KERAS_PYTHON="/anaconda/envs/py35/bin/python3.5"
 
 # Switch on RStudio Server
 
