11b14e6ed4cc511e671e315169d09c83ab5aa03b
Zhang Le <yueguoguo@users.noreply.github.com>
2017-07-04 14:48:33 +0800

    Update script.R

--- demos/demo-5/script.R
+++ demos/demo-5/script.R
@@ -9,13 +9,20 @@ devtools::install_github("rstudio/keras")
 
 # create the keras.json config file at ~/.keras.
 
-dir.create("~/.keras")
-file.create("~/.keras/keras.json", overwrite=TRUE)
+user <- Sys.getenv("USER")
+
+home_path <- file.path("/home", user)
+keras_path <- file.path(home_path, ".keras")
+keras_file <- file.path(keras_path, "keras.json")
+profile_file <- file.path(home_path, ".Rprofile")
+
+dir.create(keras_path)
+file.create(keras_file, overwrite=TRUE)
 keras_json <- '{"floatx":"float32","image_data_format":"channels_last","epsilon":1e-07,"backend":"cntk"}'
-writeLines(keras_json, con="~/.keras/keras.json")
+writeLines(keras_json, con=keras_file)
 
 # create a .Rprofile at ~
 
-file.create("~/.Rprofile", overwrite=TRUE)
+file.create(profile_file, overwrite=TRUE)
 r_profile <- 'Sys.setenv(KERAS_BACKEND="cntk"); Sys.setenv(KERAS_PYTHON="/anaconda/envs/py35/bin/python3.5")'
-writeLines(r_profile, con="~/.Rprofile")
+writeLines(r_profile, con=profile_file)
