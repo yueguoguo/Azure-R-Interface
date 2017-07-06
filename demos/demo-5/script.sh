04faa61a7540788ba92aa9627e4aa891c3ddbda8
Zhang Le <yueguoguo@users.noreply.github.com>
2017-07-06 16:47:54 +0800

    Update script.sh

--- demos/demo-5/script.sh
+++ demos/demo-5/script.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-# install needed R libraries.
+# install R libraries.
 
 Rscript -e 'library(devtools);devtools::install_github("rstudio/reticulate");devtools::install_github("gaborcsardi/debugme");source("https://install-github.me/MangoTheCat/processx");devtools::install_github("rstudio/keras")'
 
 # create keras config json file.
 
 mkdir /etc/skel/.keras
-# cat > ~/.keras/kears.json << EOF
-# {
-#    "floatx":"float32",
-#    "image_data_format":"channels_last",
-#    "epsilon":1e-01,
-#    "backend":"cntk"
-# }
-# EOF
-
 echo '{"floatx":"float32","image_data_format":"channels_last","epsilon":1e-07,"backend":"cntk"}' > /etc/skel/.keras/keras.json
 
 # export environment variables.
-
-# cat > .Rprofile << EOF
-# Sys.setenv(KERAS_BACKEND="cntk")
-# Sys.setenv(KERAS_PYTHON="/anaconda/envs/py35/bin/python")
-# EOF
 
 echo 'Sys.setenv(KERAS_BACKEND="cntk")' > /etc/skel/.Rprofile
 echo 'Sys.setenv(KERAS_PYTHON="/anaconda/envs/py35/bin/python3.5")' >> /etc/skel/.Rprofile
@@ -36,7 +22,7 @@ for u in ${USR}; do
   DBASE="/home/$u/"
   
   cp -rf /etc/skel/.keras ${DBASE}/
-  cp cat /etc/skel/.Rprofile >> ${DBASE}/.Rprofile
+  cat /etc/skel/.Rprofile >> ${DBASE}/.Rprofile
   
   chown -R $u.$u ${DBASE}/.keras
   chown -R $u.$u ${DBASE}/.Rprofile
